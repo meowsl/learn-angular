cbdc7672fdb3809c279100ddd6000cb6a240db5e
meowsl <vlafrank@ya.ru>
2024-09-22 16:44:02 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { FormComponent } from './auth/form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    FormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
